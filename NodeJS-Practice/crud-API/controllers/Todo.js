742ef48de6a082e6784a068502b40664e11d4601
akshay0077 <kherakshay007@gmail.com>
2024-01-11 18:09:41 +0530

    undo the code

--- NodeJS-Practice/crud-API/controllers/Todo.js
+++
@@ -1,8 +0,0 @@
-const getTodos = (req, res) => {
-  res.send("I am the get todos route");
-};
-
-const createTodo = (req, res) => {
-    res.send("I am CreateTodos")
-}
-export { getTodos,createTodo};
---
+++ Readme.md
@@ -0,0 +1,3 @@
+1. Focus on code quality and optimize code - resource : medium blog
+2. write down proper slack msg means write down short msg
+3. move forward in to React Basics
